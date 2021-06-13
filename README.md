309cafd31249b1ade41ef9c2cb5c123222f4c83c
Daniel Tran <trandaniel53@gmail.com>
2021-06-12 23:47:31 -0400

    Add environment variable reading in index.js as well as more links in README

--- README.md
+++ README.md
@@ -19,5 +19,13 @@ gcloud init
 gcloud functions deploy sendToNotion --entry-point toNotion --runtime nodejs14
 ```
 
+You will also need an .env file with the following values:
+- TBA
+
+## links
+
+https://medium.com/google-cloud/debugging-node-google-cloud-functions-locally-in-vs-code-e6b912eb3f84
+https://medium.com/developer-secrets/storing-credentials-the-right-way-78074ae21727
+
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -4,11 +4,21 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
+ let dotenv = require('dotenv');
+
+ // if .env file is located in root directory
+ dotenv.config()
+
 exports.toNotion = (req, res) => {
+
+  req.body = {
+    "color" : process.env.COLOR
+  }
+
   let message = {
-    "query" : req.query,
-    "request" : req.body,
+    "color" : req.body.color,
     "default message": 'Hello my world!'
   };
   res.status(200).send(message);
+  
 };
