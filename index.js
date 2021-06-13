4a755108d833edd169f1f550cd94e1ac409817ce
Daniel Tran <trandaniel53@gmail.com>
2021-06-12 21:34:40 -0400

    Initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+  let message = {
+    "query" : req.query,
+    "request" : req.body,
+    "default message": 'Hello World!'
+  };
+  res.status(200).send(message);
+};
